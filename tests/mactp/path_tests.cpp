113ba7e840dc78ce1d897717348d36b3ef30b49a
Alex Schutz <74842472+alex-schutz@users.noreply.github.com>
2024-03-17 15:32:31 +0000

    ✅ Add larger test case

--- tests/mactp/path_tests.cpp
+++ tests/mactp/path_tests.cpp
@@ -5,9 +5,9 @@
 
 using namespace CTP;
 
-class MockGraph : public ShortestPathFasterAlgorithm {
+class SmallGraph : public ShortestPathFasterAlgorithm {
  public:
-  MockGraph() = default;
+  SmallGraph() = default;
 
   std::vector<std::tuple<int, double, int>> getEdges(int node) const override {
     if (node == 1) {
@@ -21,8 +21,38 @@ class MockGraph : public ShortestPathFasterAlgorithm {
   }
 };
 
+class LargeGraph : public ShortestPathFasterAlgorithm {
+ public:
+  LargeGraph() = default;
+
+  std::vector<std::tuple<int, double, int>> getEdges(int node) const override {
+    if (node == 1) {
+      return {{2, 3.0, 1}, {3, -2.0, 1}, {4, 5.0, 1}, {7, 2.0, 1}};
+    } else if (node == 2) {
+      return {{3, 2.0, 2}, {5, 1.0, 2}, {6, -1.0, 2}};
+    } else if (node == 3) {
+      return {{4, 2.0, 3}, {6, 3.0, 3}};
+    } else if (node == 4) {
+      return {{5, -1.0, 4}, {7, 4.0, 4}, {9, 3.0, 4}, {3, 2.0, 4}};
+    } else if (node == 5) {
+      return {{6, 2.0, 5}, {8, -2.0, 5}, {9, 1.0, 5}, {2, -1.0, 5}};
+    } else if (node == 6) {
+      return {{7, 1.0, 6}, {9, 3.0, 6}};
+    } else if (node == 7) {
+      return {{8, 1.0, 7}, {10, 2.0, 7}, {1, 2.0, 7}};
+    } else if (node == 8) {
+      return {{9, 4.0, 8}, {10, 3.0, 8}};
+    } else if (node == 9) {
+      return {{10, 2.0, 9}, {4, 3.0, 9}};
+    } else if (node == 10) {
+      return {{1, 2.0, 10}, {2, 1.0, 10}, {6, -1.0, 10}};
+    }
+    return {};
+  }
+};
+
 TEST(ShortestPathFasterAlgorithmTest, Constructor) {
-  MockGraph graph;
+  SmallGraph graph;
   {
     const auto [costs, pred] = graph.calculate(1, 10);
     EXPECT_EQ(costs.size(), 3);
@@ -50,10 +80,25 @@ TEST(ShortestPathFasterAlgorithmTest, Constructor) {
     EXPECT_EQ(costs.size(), 1);
     EXPECT_EQ(costs.at(4), 0.0);
   }
+  {
+    LargeGraph lgraph;
+    const auto [costs, pred] = lgraph.calculate(1, 20);
+    EXPECT_EQ(costs.size(), 10);
+    EXPECT_EQ(costs.at(1), 0.0);
+    EXPECT_EQ(costs.at(2), -2.0);
+    EXPECT_EQ(costs.at(3), -2.0);
+    EXPECT_EQ(costs.at(4), 0.0);
+    EXPECT_EQ(costs.at(5), -1.0);
+    EXPECT_EQ(costs.at(6), -3.0);
+    EXPECT_EQ(costs.at(7), -2.0);
+    EXPECT_EQ(costs.at(8), -3.0);
+    EXPECT_EQ(costs.at(9), 0.0);
+    EXPECT_EQ(costs.at(10), 0.0);
+  }
 }
 
 TEST(ShortestPathFasterAlgorithmTest, ReconstructPath) {
-  MockGraph graph;
+  SmallGraph graph;
   const auto [costs, pred] = graph.calculate(1, 10);
   const auto path = graph.reconstructPath(3, pred);
   EXPECT_EQ(path.size(), 3);
